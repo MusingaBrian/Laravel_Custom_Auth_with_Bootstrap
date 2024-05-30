535fa2618362b12d4702b894b6d0bc217b37154f
MusingaBrian <musingabrians@gmail.com>
2024-05-30 14:57:44 +0300

    Implement route group to redirect un authenticated users

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,10 @@ Route::get('register', [AuthController::class, 'register'])->name('register');
 Route::post('register', [AuthController::class, 'registerPost'])->name('register.post');
 
 Route::get('logout', [AuthController::class, 'logOut'])->name('logout');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('profile', function () {
+        return 'Profile resource';
+    })->name('profile');
+});
