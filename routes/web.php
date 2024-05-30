0b943bfe9aade7c4dbdc0247432f315fe7238e94
MusingaBrian <musingabrians@gmail.com>
2024-05-30 10:53:52 +0300

    Add the User Controller And Login Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome_to_custom_auth');
 });
+
+Route::get('login', [UserController::class])->name('login');
+Route::post('login', [UserController::class])->name('login.post');
